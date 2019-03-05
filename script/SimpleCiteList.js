8df5371c9cb8eaacf207151d5c22344089606b1f
Leejun Liao <liaoleejun@163.com>
2019-03-05 15:09:00 +0800

    解耦 & SimpleWritingSystem重名为SimpleCombine
    * 重大更新: Tooltipcontent上可以有Tooltip

--- script/SimpleCiteList.js
+++ script/SimpleCiteList.js
@@ -1,41 +1,39 @@
 $(document).ready(
     function () {
-        /* 1. 引用排序: 按照引用在 HTML 文档中出现顺序, 把引用的 data-ref 生成无重复值
-              的数组 citesOrdered */
-        let cites = $("[data-ct]");
-        // citesOrdered 是由 data-ref 构成的无重复数组，按照在HTML文档中出现的顺序
-        let citesOrdered = [];
+        /* 1. 把引用进行排序 */
+        let cites = $(".cite");
+        // citesOrdered 是由class为cite的元素，的href值，构成的无重复数组（按照在HTML文档中出现的顺序）
+        let citesRefOrdered = [];
         for (let i = 0; i < cites.length; i++) {
-            let dataRef = cites[i].getAttribute('data-ct');
+            let citeRef = cites[i].getAttribute('href');
             // array push only if not exist
-            citesOrdered.indexOf(dataRef) === -1 ? citesOrdered.push(dataRef) :
-                console.log(dataRef + " already exists");
+            citesRefOrdered.indexOf(citeRef) === -1 ? citesRefOrdered.push(citeRef) : console.log(citeRef + " already exists");
         }
 
-        /* 2. 把 HTML 文档中的引用, 按照上一步生成的数组的顺序给标上号*/
+        /* 2. 把 HTML 文档中的引用, 按照上一步生成的数组的顺序给标上号 */
         for (let i = 0; i < cites.length; i++) {
-            let dataRef = cites[i].getAttribute('data-ct');
+            let citeRef = cites[i].getAttribute('href');
             // 给引用标上号（注意数组是从0开始下标，所以加1）
-            let num = citesOrdered.indexOf(dataRef) + 1;
-            cites[i].innerHTML = '<a>[' + num + ']</a>';
-            // 引用设置锚点
-            cites[i].firstChild.setAttribute('href', '#' + dataRef);
+            let num = citesRefOrdered.indexOf(citeRef) + 1;
+            cites[i].innerHTML = '[' + num + ']';
         }
 
-        /* 3. 生成排好序的 Cite list */
+        /* 3. 生成排好序的引用列表，附在最后 */
         // 使用cite_list_outside风格, 当大于999时, 就会溢出边界。所以设定了不同的cite list风格。（可以通过设置citesOrdered.length < 11这样来测试看效果）
-        // 还可以直接用js生成, 而不用css的list来实现
-        if (citesOrdered.length < 999) {
-            $(document.body).append("<div class='cite_list_outside'><h2>References</h2><ol id='cites'></ol></div>");
-        } else {
-            $(document.body).append("<div class='cite_list_inside'><h2>References</h2><ol id='cites'></ol></div>");
-        }
-        for (let i = 0; i < citesOrdered.length; i++) {
-            let id = citesOrdered[i];
-            let ttt = document.createElement('li');
-            // todo: Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
-            ttt.appendChild(document.getElementById(id));
-            document.getElementById("cites").appendChild(ttt);
+        // 还可以直接用js生成, 而不用css的list来实现 TODO
+        if (citesRefOrdered.length > 0) {
+            if (citesRefOrdered.length < 999) {
+                $(document.body).append("<div class='list_outside'><h2>References</h2><ol id='cites-append'></ol></div>");
+            } else {
+                $(document.body).append("<div class='list_inside'><h2>References</h2><ol id='cites-append'></ol></div>");
+            }
+            for (let i = 0; i < citesRefOrdered.length; i++) {
+                let id = citesRefOrdered[i].substring(1);
+                let item = document.createElement('li');
+                // todo: Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+                item.appendChild(document.getElementById(id));
+                document.getElementById("cites-append").appendChild(item);
+            }
         }
     }
 );
--- script/SimpleCombine.js
+++ script/SimpleCombine.js
@@ -1,22 +1,22 @@
 $(document).ready(
     function () {
-        let cites = $("[data-ct]");
+        let cites = $("[data-c]");
         for (let i = 0; i < cites.length; i++) {
-            let dataRef = cites[i].getAttribute('data-ct');
+            let dataRef = cites[i].getAttribute('data-c');
             // Add the class name "tooltip", add the attr "data-ref", 因为
             // SimpleTooltip.js是以类名为tooltip, 属性data-ref来选择的
-            cites[i].classList.add("tooltip");
-            $(cites[i]).attr("data-ref", dataRef);
+            cites[i].classList.add("tooltip", "cite");
+            $(cites[i]).attr("href", "#" + dataRef);
         }
 
-        let descs = $("[data-dt]");
+        let descs = $("[data-d]");
         for (let i = 0; i < descs.length; i++) {
-            let dataRef = descs[i].getAttribute('data-dt');
+            let dataRef = descs[i].getAttribute('data-d');
             let concept = descs[i].innerHTML;
             // Add the class name "tooltip", add the attr "data-ref", 因为
             // SimpleTooltip.js是以类名为tooltip, 属性data-ref来选择的
-            descs[i].classList.add("tooltip");
-            $(descs[i]).attr("data-ref", dataRef);
+            descs[i].classList.add("tooltip", "description");
+            $(descs[i]).attr("href", "#" + dataRef);
         }
     }
 );
